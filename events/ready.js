30c2cdc1e241f5353d7d99be76b1bc0060ff0dbb
Myklae <82762285+Myklae@users.noreply.github.com>
2021-08-18 17:46:34 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const moment = require('moment');
+const chalk = require('chalk');
+const { prefix } = require('../ayarlar.json')
+const amed = new Discord.Client();
+  
+  amed.user.setStatus("dnd"); 
+  
